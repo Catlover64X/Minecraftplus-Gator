e1b0809b55e163aef5edc38564adbc38cec17323
Catlover64X <103081474+Catlover64X@users.noreply.github.com>
2022-04-09 20:05:28 -0400

    Added Cat Jesus's Blood

---
+++ src/main/java/net/mcreator/minecraftgators/init/MinecraftGatorsModSounds.java
@@ -0,0 +1,29 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.minecraftgators.init;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class MinecraftGatorsModSounds {
+	public static Map<ResourceLocation, SoundEvent> REGISTRY = new HashMap<>();
+	static {
+		REGISTRY.put(new ResourceLocation("minecraft_gators", "disc_sound"), new SoundEvent(new ResourceLocation("minecraft_gators", "disc_sound")));
+	}
+
+	@SubscribeEvent
+	public static void registerSounds(RegistryEvent.Register<SoundEvent> event) {
+		for (Map.Entry<ResourceLocation, SoundEvent> sound : REGISTRY.entrySet())
+			event.getRegistry().register(sound.getValue().setRegistryName(sound.getKey()));
+	}
+}
---
+++ src/main/java/net/mcreator/minecraftgators/recipes/brewing/CatJesusBloodRecipeBrewingRecipe.java
@@ -0,0 +1,44 @@
+
+package net.mcreator.minecraftgators.recipes.brewing;
+
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.brewing.IBrewingRecipe;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+
+import net.mcreator.minecraftgators.init.MinecraftGatorsModPotions;
+import net.mcreator.minecraftgators.init.MinecraftGatorsModItems;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class CatJesusBloodRecipeBrewingRecipe implements IBrewingRecipe {
+	@SubscribeEvent
+	public static void init(FMLCommonSetupEvent event) {
+		event.enqueueWork(() -> BrewingRecipeRegistry.addRecipe(new CatJesusBloodRecipeBrewingRecipe()));
+	}
+
+	@Override
+	public boolean isInput(ItemStack input) {
+		Item inputItem = input.getItem();
+		return (inputItem == Items.POTION || inputItem == Items.SPLASH_POTION || inputItem == Items.LINGERING_POTION)
+				&& PotionUtils.getPotion(input) == MinecraftGatorsModPotions.CAT_NIP;
+	}
+
+	@Override
+	public boolean isIngredient(ItemStack ingredient) {
+		return ingredient.getItem() == MinecraftGatorsModItems.CAT_JESUS_DUST_DUST;
+	}
+
+	@Override
+	public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
+		if (isInput(input) && isIngredient(ingredient)) {
+			return PotionUtils.setPotion(new ItemStack(input.getItem()), MinecraftGatorsModPotions.CAT_JESUSS_BLOOD);
+		}
+		return ItemStack.EMPTY;
+	}
+}
