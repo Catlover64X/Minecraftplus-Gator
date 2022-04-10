77f3830c007066f250f46e445e9f725348d6d4c3
AllSuperHeroes <103081876+AllSuperHeroes@users.noreply.github.com>
2022-04-09 20:13:58 -0400

    fixed things again

--- src/main/java/net/mcreator/minecraftgators/recipes/brewing/CatJesusBloodRecipeBrewingRecipe.java
+++ src/main/java/net/mcreator/minecraftgators/recipes/brewing/CatJesusBloodRecipeBrewingRecipe.java
@@ -1,9 +1,22 @@
 
 package net.mcreator.minecraftgators.recipes.brewing;
 
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.brewing.IBrewingRecipe;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+
+import net.minecraft.world.item.alchemy.PotionUtils;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+
+import net.mcreator.minecraftgators.init.MinecraftGatorsModPotions;
+import net.mcreator.minecraftgators.init.MinecraftGatorsModItems;
+
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CatJesusBloodRecipeBrewingRecipe implements IBrewingRecipe {
-
 	@SubscribeEvent
 	public static void init(FMLCommonSetupEvent event) {
 		event.enqueueWork(() -> BrewingRecipeRegistry.addRecipe(new CatJesusBloodRecipeBrewingRecipe()));
@@ -24,9 +37,8 @@ public class CatJesusBloodRecipeBrewingRecipe implements IBrewingRecipe {
 	@Override
 	public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
 		if (isInput(input) && isIngredient(ingredient)) {
-			return PotionUtils.setPotion(new ItemStack(input.getItem()), MinecraftGatorsModPotions.DELETED_MOD_ELEMENT);
+			return PotionUtils.setPotion(new ItemStack(input.getItem()), MinecraftGatorsModPotions.CAT_JESUSS_BLOOD);
 		}
 		return ItemStack.EMPTY;
 	}
-
 }
