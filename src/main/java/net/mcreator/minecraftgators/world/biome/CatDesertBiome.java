a6d6e75f21f6d1f5f1413192be134cac623fa709
AllSuperHeroes <103081876+AllSuperHeroes@users.noreply.github.com>
2022-04-09 20:12:52 -0400

    fixed things

--- src/main/java/net/mcreator/minecraftgators/world/biome/CatDesertBiome.java
+++ src/main/java/net/mcreator/minecraftgators/world/biome/CatDesertBiome.java
@@ -1,11 +1,24 @@
 
 package net.mcreator.minecraftgators.world.biome;
 
-import net.minecraftforge.common.BiomeManager;
-import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.level.levelgen.surfacebuilders.SurfaceBuilderBaseConfiguration;
+import net.minecraft.world.level.levelgen.surfacebuilders.SurfaceBuilder;
+import net.minecraft.world.level.levelgen.surfacebuilders.ConfiguredSurfaceBuilder;
+import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraft.world.level.biome.BiomeSpecialEffects;
+import net.minecraft.world.level.biome.BiomeGenerationSettings;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.data.worldgen.BiomeDefaultFeatures;
+import net.minecraft.data.BuiltinRegistries;
+import net.minecraft.core.Registry;
+
+import net.mcreator.minecraftgators.init.MinecraftGatorsModBlocks;
+import net.mcreator.minecraftgators.MinecraftGatorsMod;
 
 public class CatDesertBiome {
-
 	private static final ConfiguredSurfaceBuilder<?> SURFACE_BUILDER = SurfaceBuilder.DEFAULT
 			.configured(new SurfaceBuilderBaseConfiguration(MinecraftGatorsModBlocks.CAT_LITTER.defaultBlockState(),
 					MinecraftGatorsModBlocks.CAT_FOOD_BLOCK.defaultBlockState(), MinecraftGatorsModBlocks.CAT_FOOD_BLOCK.defaultBlockState()));
@@ -13,14 +26,10 @@ public class CatDesertBiome {
 	public static Biome createBiome() {
 		BiomeSpecialEffects effects = new BiomeSpecialEffects.Builder().fogColor(12638463).waterColor(4159204).waterFogColor(329011).skyColor(7972607)
 				.foliageColorOverride(10387789).grassColorOverride(9470285).build();
-
 		BiomeGenerationSettings.Builder biomeGenerationSettings = new BiomeGenerationSettings.Builder().surfaceBuilder(SURFACE_BUILDER);
-
 		BiomeDefaultFeatures.addDesertVegetation(biomeGenerationSettings);
-
 		MobSpawnSettings.Builder mobSpawnInfo = new MobSpawnSettings.Builder().setPlayerCanSpawn();
 		mobSpawnInfo.addSpawn(MobCategory.MONSTER, new MobSpawnSettings.SpawnerData(EntityType.HUSK, 20, 4, 4));
-
 		return new Biome.BiomeBuilder().precipitation(Biome.Precipitation.NONE).biomeCategory(Biome.BiomeCategory.DESERT).depth(0.1f).scale(0.2f)
 				.temperature(2f).downfall(0f).specialEffects(effects).mobSpawnSettings(mobSpawnInfo.build())
 				.generationSettings(biomeGenerationSettings.build()).build();
@@ -29,7 +38,5 @@ public class CatDesertBiome {
 	public static void init() {
 		Registry.register(BuiltinRegistries.CONFIGURED_SURFACE_BUILDER, new ResourceLocation(MinecraftGatorsMod.MODID, "cat_desert"),
 				SURFACE_BUILDER);
-
 	}
-
 }
